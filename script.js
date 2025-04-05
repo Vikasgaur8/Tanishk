c42cee752a7e659b995c88d6fd73db9c7930ba2c
Vikasgaur8 <vikasgaur9950@gmail.com>
2025-04-05 15:59:23 +0530

    test comment

--- script.js
+++ script.js
@@ -47,16 +47,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Sample properties data for dynamic cards
 const residential = [
-  {
-    image: "images/images/m3mfullimg.jpg",
-    image2: "images/images/m3m.png",
-    name: "TEStt",
-    type: "Commercial",
-    location:
-      "M3M International Financial Center, Sector-66, Golf Course Road (Extn.), Gurugram-122101, Haryana, India",
-    // price: "On Request",
-    link: "/Tanishk/knowMorePage/knowMore.html",
-  },
+  // {
+  //   image: "images/images/m3mfullimg.jpg",
+  //   image2: "images/images/m3m.png",
+  //   name: "TEStt",
+  //   type: "Commercial",
+  //   location:
+  //     "M3M International Financial Center, Sector-66, Golf Course Road (Extn.), Gurugram-122101, Haryana, India",
+  //   // price: "On Request",
+  //   link: "/Tanishk/knowMorePage/knowMore.html",
+  // },
   {
     // image: "https://via.placeholder.com/400x180", // Replace with actual image URLs
     image: "images/images/Signature Global.jpg", // Replace with actual image URLs
